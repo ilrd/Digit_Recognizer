bd6b072d1b57300b90b7c47bd3086fe09768f997
Illia Herasymenko <illia.cgerasimenko@gmail.com>
2020-12-21 01:12:34 +0200

    Added files path selection for docker implementation

--- src/modeling/prediction.py
+++ src/modeling/prediction.py
@@ -2,18 +2,26 @@ from PIL import Image
 import numpy as np
 from tensorflow.keras.models import load_model
 import tensorflow as tf
+import os
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
 
-path = '/home/ilolio/PycharmProjects/Digit_Recognizer/src/modeling/saved_models/best_model.h5'
+
+path = './modeling/saved_models/best_model.h5'
+if not os.path.exists(path):
+    path = './src/modeling/saved_models/best_model.h5'  # for docker implementation
+
 model = load_model(path)
 # Mock prediction to turn on tensorflow on the startup
 pred = model.predict(np.zeros((1, 28, 28, 1)))
 
 
 def pred_digit():
-    im = Image.open(f'/home/ilolio/PycharmProjects/Digit_Recognizer/src/tmp/user_digit.png')
+    img_path = './tmp/user_digit.png'
+    if not os.path.exists(img_path):
+        img_path = './src/tmp/user_digit.png'  # for docker implementation
+    im = Image.open(img_path)
     im = im.resize((28, 28))
     im_array = np.array(im)
     im_array = np.array([[np.average(j) for j in im_array[i]] for i in range(len(im_array))])
